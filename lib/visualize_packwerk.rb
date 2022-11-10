8d29366edfedb82bebb6669fc5f3c1eed9afdb04
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-11-10 10:48:16 -0500

    Simplify invocation of behavior (#9)

--- lib/visualize_packwerk.rb
+++ lib/visualize_packwerk.rb
@@ -1,16 +1,28 @@
 # typed: strict
 
-module VisualizePackwerk
-  require 'visualize_packwerk/railtie' if defined?(Rails)
-  require 'parse_packwerk'
-  require 'code_ownership'
-  require 'graphviz'
+require 'parse_packwerk'
+require 'code_ownership'
+require 'graphviz'
+require 'sorbet-runtime'
 
-  require 'visualize_packwerk/node_interface'
-  require 'visualize_packwerk/graph_interface'
-  require 'visualize_packwerk/team_node'
-  require 'visualize_packwerk/package_node'
-  require 'visualize_packwerk/team_graph'
-  require 'visualize_packwerk/package_graph'
-  require 'visualize_packwerk/package_relationships'
+require 'visualize_packwerk/node_interface'
+require 'visualize_packwerk/graph_interface'
+require 'visualize_packwerk/team_node'
+require 'visualize_packwerk/package_node'
+require 'visualize_packwerk/team_graph'
+require 'visualize_packwerk/package_graph'
+require 'visualize_packwerk/package_relationships'
+
+module VisualizePackwerk
+  extend T::Sig
+
+  sig { params(packages: T::Array[ParsePackwerk::Package]).void }
+  def self.package_graph!(packages)
+    PackageRelationships.new.create_package_graph!(packages)
+  end
+
+  sig { params(teams: T::Array[CodeTeams::Team]).void }
+  def self.team_graph!(teams)
+    PackageRelationships.new.create_team_graph!(teams)
+  end
 end
