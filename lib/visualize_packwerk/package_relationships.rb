0fd53997bfaff56647928875ef67a07cabcf2326
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-05-02 10:55:43 -0400

    Make output more accessible by adding legend, title, and colorblind-friendly shapes (#15)
    * Make output more accessible by adding legend, title, and colorblind-friendly shapes
    
    * add image to readme
    
    * update readme
    
    * Bump versions

--- lib/visualize_packwerk/package_relationships.rb
+++ lib/visualize_packwerk/package_relationships.rb
@@ -45,7 +45,15 @@ module VisualizePackwerk
       # https://graphviz.org/docs/layouts/
       default_layout = :dot
       # other_layout = :sfdp
-      graphviz_graph = GraphViz.new(:G, type: :digraph, dpi: 100, layout: default_layout)
+      graphviz_graph = GraphViz.new(
+        :G,
+        type: :digraph,
+        dpi: 100,
+        layout: default_layout,
+        label: "Visualization of #{node_names.count} packs, generated using `bin/packs`",
+        fontsize: 24,
+        labelloc: "t",
+      )
 
       # Create graph nodes
       graphviz_nodes = T.let({}, T::Hash[String, GraphViz::Node])
@@ -56,35 +64,31 @@ module VisualizePackwerk
         graphviz_nodes[node.name] = add_node(node, graphviz_graph)
       end
 
-      max_edge_width = 10
-
       # Draw all edges
       nodes_to_draw.each do |node|
         node.dependencies.each do |to_node|
           next unless node_names.include?(to_node)
 
-          graphviz_graph.add_edges(
-            graphviz_nodes[node.name],
-            graphviz_nodes[to_node],
-            { color: 'darkgreen' }
+          add_dependency(
+            graph: graphviz_graph,
+            node1: T.must(graphviz_nodes[node.name]),
+            node2: T.must(graphviz_nodes[to_node]),
           )
         end
 
         node.violations_by_node_name.each do |to_node_name, violation_count|
           next unless node_names.include?(to_node_name)
 
-          edge_width = [
-            [(violation_count / 5).to_i, 1].max, # rubocop:disable Lint/NumberConversion
-            max_edge_width,
-          ].min
-
-          graphviz_graph.add_edges(
-            graphviz_nodes[node.name],
-            graphviz_nodes[to_node_name],
-            { color: 'red', penwidth: edge_width }
+          add_violation(
+            graph: graphviz_graph,
+            node1: T.must(graphviz_nodes[node.name]),
+            node2: T.must(graphviz_nodes[to_node_name]),
+            violation_count: violation_count
           )
         end
       end
+
+      add_legend(graphviz_graph)
 
       # Save graph to filesystem
       puts "Outputting to: #{OUTPUT_FILENAME}"
@@ -105,6 +109,60 @@ module VisualizePackwerk
       }
 
       graph.add_nodes(node.name, **node_options)
+    end
+
+    sig { params(graph: GraphViz).void }
+    def add_legend(graph)
+      legend = graph.add_graph("legend")
+
+      # This commented out code was used to generate an image that I edited by hand.
+      # I was unable to figure out how to:
+      # - put a box around the legend
+      # - layout the node pairs in vertical order
+      # - give it a title
+      # So I just generated this using graphviz and then pulled the image in.
+      # a_node = legend.add_nodes("packs/a")
+      # b_node = legend.add_nodes("packs/b")
+      # c_node = legend.add_nodes("packs/c")
+      # d_node = legend.add_nodes("packs/d")
+      # e_node = legend.add_nodes("packs/e")
+      # f_node = legend.add_nodes("packs/f")
+
+      # add_dependency(graph: legend, node1: a_node, node2: b_node, label: 'Dependency in package.yml')
+      # add_violation(graph: legend, node1: c_node, node2: d_node, violation_count: 1, label: 'Violations (few)')
+      # add_violation(graph: legend, node1: e_node, node2: f_node, violation_count: 30, label: 'Violations (many)')
+
+      image = legend.add_node("",
+        shape: "image",
+        image: Pathname.new(__dir__).join("./legend.png").to_s,
+      )
+    end
+
+    sig { params(graph: GraphViz, node1: GraphViz::Node, node2: GraphViz::Node, violation_count: Integer, label: T.nilable(String)).void }
+    def add_violation(graph:, node1:, node2:, violation_count:, label: nil)
+      max_edge_width = 10
+
+      edge_width = [
+        [(violation_count / 5).to_i, 1].max, # rubocop:disable Lint/NumberConversion
+        max_edge_width,
+      ].min
+
+      opts = { color: 'red', style: 'dashed', penwidth: edge_width }
+      if label
+        opts.merge!(label: label)
+      end
+
+      graph.add_edges(node1, node2, opts)
+    end
+
+    sig { params(graph: GraphViz, node1: GraphViz::Node, node2: GraphViz::Node, label: T.nilable(String)).void }
+    def add_dependency(graph:, node1:, node2:, label: nil)
+      opts = { color: 'darkgreen' }
+      if label
+        opts.merge!(label: label)
+      end
+
+      graph.add_edges(node1, node2, opts)
     end
   end
 
