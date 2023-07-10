eeda3fb52398753200e6bad34f457a54476edcae
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-07-10 14:26:28 -0600

    Build the graph with ERB

--- lib/visualize_packwerk.rb
+++ lib/visualize_packwerk.rb
@@ -2,28 +2,24 @@
 
 require 'packs'
 require 'parse_packwerk'
-require 'code_ownership'
 require 'graphviz'
-require 'sorbet-runtime'
 
-require 'visualize_packwerk/node_interface'
-require 'visualize_packwerk/graph_interface'
-require 'visualize_packwerk/team_node'
-require 'visualize_packwerk/package_node'
-require 'visualize_packwerk/team_graph'
-require 'visualize_packwerk/package_graph'
-require 'visualize_packwerk/package_relationships'
+require 'erb'
 
 module VisualizePackwerk
   extend T::Sig
 
-  sig { params(packages: T::Array[Packs::Pack]).void }
-  def self.package_graph!(packages)
-    PackageRelationships.new.create_package_graph!(packages)
-  end
+  sig { params(all_packages: T::Array[Packs::Pack]).void }
+  def self.package_graph!(all_packages)
+    config = ParsePackwerk::Configuration.fetch
 
-  sig { params(teams: T::Array[CodeTeams::Team]).void }
-  def self.team_graph!(teams)
-    PackageRelationships.new.create_team_graph!(teams)
+    grouped_packages = all_packages.inject({}) do |result, package|
+      result[package.config['layer'] || "NotInLayer"] ||= []
+      result[package.config['layer'] || "NotInLayer"] << package
+      result
+    end
+    file = File.open(File.expand_path File.dirname(__FILE__) + "/graph.dot.erb")
+    template = ERB.new(file.read)
+    puts template.result(binding)
   end
 end
