1ae2c69d24add16506f26c281f4dc33edec3198d
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-07-10 21:30:28 -0600

    Get rid of other stuff

--- sorbet/tapioca/require.rb
+++
@@ -1,7 +0,0 @@
-# typed: true
-# frozen_string_literal: true
-
-# Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
-require 'rake'
-require 'code_teams'
-require 'visualize_packwerk'
