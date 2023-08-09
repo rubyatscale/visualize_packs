9de72a206001ae0490a53ccfefd06287dc998dfb
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-08-09 13:12:42 -0400

    Revert "Support a whole bunch of command line params"
    This reverts commit 6f915720e460ff63dd6767951f241a6ca693def2.

--- lib/visualize_packwerk.rb
+++ lib/visualize_packwerk.rb
@@ -1,43 +1,25 @@
 # typed: strict
 
-require 'erb'
 require 'packs'
 require 'parse_packwerk'
+require 'graphviz'
+
+require 'erb'
 
 module VisualizePackwerk
+  extend T::Sig
 
-  def self.package_graph!(options)
+  sig { params(all_packages: T::Array[Packs::Pack]).void }
+  def self.package_graph!(all_packages)
     config = ParsePackwerk::Configuration.fetch
-    
-    all_packages = filtered(Packs.all, options.only_package) 
-    all_package_names = all_packages.map &:name
 
-    grouped_packages = config.raw['architecture_layers'].inject({"NotInLayer" => []}) do |result, key|
-      result[key] = []
+    grouped_packages = all_packages.inject({}) do |result, package|
+      result[package.config['layer'] || "NotInLayer"] ||= []
+      result[package.config['layer'] || "NotInLayer"] << package
       result
     end
-    all_packages.each do |package|
-      grouped_packages[package.config['layer'] || "NotInLayer"] << package
-    end
-
     file = File.open(File.expand_path File.dirname(__FILE__) + "/graph.dot.erb")
     template = ERB.new(file.read)
     puts template.result(binding)
-  end
-
-  private 
-
-  def self.filtered(all_packages, filter_package)
-    packages = all_packages.map { |pack| ParsePackwerk.find(pack.name) }
-    return packages unless filter_package
-
-    result = [filter_package]
-    result += packages.select{ |p| p.dependencies.include? filter_package }.map { |pack| pack.name }
-    result += ParsePackwerk.find(filter_package).dependencies
-    result += packages.select{ |p| p.violations.map(&:to_package_name).include? filter_package }.map { |pack| pack.name }
-    result += ParsePackwerk.find(filter_package).violations.map(&:to_package_name)
-    result = result.uniq
-
-    result.map { |pack_name| ParsePackwerk.find(pack_name) }
   end
 end
