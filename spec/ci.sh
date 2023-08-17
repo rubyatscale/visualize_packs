771a4aa7269c9db38a243883f67a567f44d8d07f
Stephan Hagemann <shageman@users.noreply.github.com>
2023-08-17 10:03:09 -0600

    Fixing cassettes. (#24)
    https://github.com/rubyatscale/visualize_packs/pull/23 should have not been allowed to merge because status checks failed. However, we had the
    Branch protection misconfigured to not require any status checks to pass
    
    I updated the branch protection setting...

--- spec/ci.sh
+++ spec/ci.sh
@@ -6,29 +6,24 @@ bundle
 
 rm -f tests/*.dot
 
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app > tests/plain.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --no-layers > tests/no_layers.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --no-dependencies > tests/no_dependencies.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --no-todos > tests/no_todos.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --no-privacy > tests/no_privacy.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --no-teams > tests/no_teams.dot
+URL="--remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app"
 
-bundle exec visualize_packs --no-dependencies --no-todos --no-privacy --no-teams > tests/only_layers.dot
-bundle exec visualize_packs --no-layers --no-dependencies --no-todos --no-privacy --no-teams > tests/no_to_all.dot
+bundle exec visualize_packs $URL > tests/plain.dot
+bundle exec visualize_packs --no-layers $URL > tests/no_layers.dot
+bundle exec visualize_packs --no-dependencies $URL > tests/no_dependencies.dot
+bundle exec visualize_packs --no-todos $URL > tests/no_todos.dot
+bundle exec visualize_packs --no-privacy $URL > tests/no_privacy.dot
+bundle exec visualize_packs --no-teams $URL > tests/no_teams.dot
+bundle exec visualize_packs --roll_nested_todos_into_top_level $URL > tests/roll_nested_todos_into_top_level.dot
+bundle exec visualize_packs --no_nested_relationships $URL > tests/no_nested_relationships.dot
 
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=. > tests/focussed_on_root.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=packs/ui > tests/focussed_on_packs_ui.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=packs/models > tests/focussed_on_packs_model.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=packs/utility > tests/focussed_on_packs_utility.dot
+bundle exec visualize_packs             --no-dependencies --no-todos --no-privacy --no-teams --no_nested_relationships $URL > tests/only_layers.dot
+bundle exec visualize_packs --no-layers --no-dependencies --no-todos --no-privacy --no-teams --no_nested_relationships $URL > tests/no_to_all.dot
 
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=. --only-edges-to-focus > tests/focussed_on_root_focus_edges.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=packs/ui --only-edges-to-focus > tests/focussed_on_packs_ui_focus_edges.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=packs/models --only-edges-to-focus > tests/focussed_on_packs_model_focus_edges.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_on=packs/utility --only-edges-to-focus > tests/focussed_on_packs_utility_focus_edges.dot
+bundle exec visualize_packs --focus_on=packs/ui $URL > tests/focussed_on_packs_ui.dot
+bundle exec visualize_packs --focus_on=packs/ui --only-edges-to-focus $URL > tests/focussed_on_packs_ui_focus_edges.dot
 
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --roll_nested_todos_into_top_level > tests/roll_nested_todos_into_top_level.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --focus_folder=packs/model > tests/focus_folder.dot
-bundle exec visualize_packs --remote-base-url=https://github.com/shageman/visualize_packwerk/tree/main/spec/sample_app --no_nested_relationships > tests/no_nested_relationships.dot
+bundle exec visualize_packs --focus_folder=packs/model $URL > tests/focus_folder.dot
 
 git status
 
