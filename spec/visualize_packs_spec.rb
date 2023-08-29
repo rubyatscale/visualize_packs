59d75636d5420eaeb94f0a4e68c36f86c4e13b58
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-08-28 22:01:58 -0600

    Make pack exclusion work with filename-style wildcards

--- spec/visualize_packs_spec.rb
+++ spec/visualize_packs_spec.rb
@@ -279,4 +279,25 @@ RSpec.describe "VisualizePacks" do
       end
     end
   end
+
+  describe '.exclude_pack?' do
+    it 'does not exclude non-matches' do
+      expect(VisualizePacks.exclude_pack?("pack", ["packs", "spack", "ack", "components"])).to be_falsy
+    end
+
+    it 'excludes matches' do
+      expect(VisualizePacks.exclude_pack?("pack", ["pack", "park"])).to be_truthy
+      expect(VisualizePacks.exclude_pack?("park", ["pack", "park"])).to be_truthy
+    end
+
+    it 'excludes matches using fnmatch' do
+      expect(VisualizePacks.exclude_pack?("component", ["pack/*"])).to be_falsy
+      expect(VisualizePacks.exclude_pack?("pack", ["pack/*"])).to be_falsy
+      expect(VisualizePacks.exclude_pack?("pack/a", ["pack/*"])).to be_truthy
+      expect(VisualizePacks.exclude_pack?("pack/a/b", ["pack/*"])).to be_truthy
+
+      expect(VisualizePacks.exclude_pack?("pack/a/b", ["pack/*/b"])).to be_truthy
+      expect(VisualizePacks.exclude_pack?("pack/a/c", ["pack/*/b"])).to be_falsy
+    end
+  end
 end
