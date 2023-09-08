480edf554bc1947583d1b5f65bf00ace934fcb37
Diego Algorta <diego@oboxodo.com>
2023-09-08 16:39:30 -0300

    Update old param version from --no-privacy to --no-privacy-boxes

--- spec/diagram_generation_helper.sh
+++ spec/diagram_generation_helper.sh
@@ -27,8 +27,8 @@ test_names+=("no_visibility"); test_params[${test_names[-1]}]="--no-visibility-a
 
 test_names+=("only_todo_types"); test_params[${test_names[-1]}]="--only-todo-types=architecture,visibility"
 test_names+=("roll_nested_into_parent_packs"); test_params[${test_names[-1]}]="--roll-nested-into-parent-packs"
-test_names+=("only_layers"); test_params[${test_names[-1]}]="--no-dependency-arrows --no-todo-arrows --no-privacy --no-teams --no-nesting-arrows"
-test_names+=("no_to_all"); test_params[${test_names[-1]}]="--title='Hide everything' --no-layers --no-visibility --no-dependency-arrows --no-todo-arrows --no-privacy --no-teams --no-nesting-arrows"
+test_names+=("only_layers"); test_params[${test_names[-1]}]="--no-dependency-arrows --no-todo-arrows --no-privacy-boxes --no-teams --no-nesting-arrows"
+test_names+=("no_to_all"); test_params[${test_names[-1]}]="--title='Hide everything' --no-layers --no-visibility --no-dependency-arrows --no-todo-arrows --no-privacy-boxes --no-teams --no-nesting-arrows"
 test_names+=("focussed_on_packs_ui"); test_params[${test_names[-1]}]="--focus-pack=packs/ui"
 test_names+=("focussed_on_packs_ui_focus_edges"); test_params[${test_names[-1]}]="--focus-pack=packs/ui --focus-pack-edge-mode=inout"
 
